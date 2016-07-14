a5203e1fa95f912a7e9ab64156e5a475ff3e1646
Nam <namnv@runsystem.net>
2016-07-14 17:35:19 +0700

    Javascript week1

---
+++ fuel/app/classes/controller/learn.php
@@ -0,0 +1,54 @@
+<?php
+
+class Controller_Learn extends Controller
+{
+	public function action_index()
+	{
+		return Response::forge(View::forge('welcome/index'));
+	}
+	public function action_Bai1(){
+		return Response::forge(View::forge('BaiTap/bai1'));
+	}
+	public function action_Bai2(){
+		return Response::forge(View::forge('BaiTap/bai2'));
+	}
+	public function action_Bai3(){
+		return Response::forge(View::forge('BaiTap/bai3'));
+	}
+	public function action_Bai4(){
+		return Response::forge(View::forge('BaiTap/bai4'));
+	}
+	public function action_Bai5(){
+		return Response::forge(View::forge('BaiTap/bai5'));
+	}
+	public function action_Bai6(){
+		return Response::forge(View::forge('BaiTap/bai6'));
+	}
+	public function action_Bai7(){
+		return Response::forge(View::forge('BaiTap/bai7'));
+	}
+	public function action_Bai8(){
+		return Response::forge(View::forge('BaiTap/bai8'));
+	}
+	public function action_Bai9(){
+		return Response::forge(View::forge('BaiTap/bai9'));
+	}
+	public function action_Bai10(){
+		return Response::forge(View::forge('BaiTap/bai10'));
+	}
+	public function action_Bai11(){
+		return Response::forge(View::forge('BaiTap/bai11'));
+	}
+	public function action_Bai12(){
+		return Response::forge(View::forge('BaiTap/bai12'));
+	}
+	public function action_Bai13(){
+		return Response::forge(View::forge('BaiTap/bai13'));
+	}
+	public function action_add(){
+		// return Response::forge('/');	
+
+		
+	}
+	
+}
